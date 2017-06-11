f7503e394b27f558d374efc66f42edde17e12e04
rubentorresbonet <rubentorresbonet@gmail.com>
2017-06-11 18:48:33 +0200

    attempt to fix compile shaders issue

--- step.sh
+++ step.sh
@@ -7,7 +7,7 @@ if [ "$(uname)" == "Darwin" ]; then
         -projectPath $project_path \
         -collection=$build_collection
 else
-    xvfb-run /opt/Unity/Editor/Unity -logfile -quit -batchmode -force-opengl \
+    xvfb-run /opt/Unity/Editor/Unity -logfile -quit -batchmode -nographics \
         -executeMethod UBS.UBSProcess.BuildFromCommandLine \
         -android-ndk=$android_ndk_home -android-sdk=$ANDROID_HOME \
         -projectPath $project_path \
