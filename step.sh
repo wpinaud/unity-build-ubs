0a05ab80a405fae29a8a1ad8d4c33c250d38d814
rubentorresbonet <rubentorresbonet@gmail.com>
2017-06-11 15:54:15 +0200

    initial commit

---
+++ step.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ "$(uname)" == "Darwin" ]; then
+    /Applications/Unity/Unity.app/Contents/MacOS/Unity -logfile -quit -batchmode -nographics \
+        -executeMethod UBS.UBSProcess.BuildFromCommandLine \
+        -android-ndk=$android_ndk_home -android-sdk=$ANDROID_HOME \
+        -projectPath $project_path \
+        -collection=$build_collection
+else
+    /opt/Unity/Editor/Unity -logfile -quit -batchmode -nographics -force-opengl \
+        -executeMethod UBS.UBSProcess.BuildFromCommandLine \
+        -android-ndk=$android_ndk_home -android-sdk=$ANDROID_HOME \
+        -projectPath $project_path \
+        -collection=$build_collection
+fi
