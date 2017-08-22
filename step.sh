1d3a1c57c2f63c8f2824e9046272c2f56f4d232e
rubentorresbonet <ruben.torres@exozet.com>
2017-08-22 19:51:08 +0200

    Update step.sh
    Added jdk home

--- step.sh
+++ step.sh
@@ -4,6 +4,7 @@ if [ "$(uname)" == "Darwin" ]; then
     /Applications/Unity/Unity.app/Contents/MacOS/Unity -logfile -quit -batchmode -nographics \
         -executeMethod UBS.UBSProcess.BuildFromCommandLine \
         -android-ndk=$android_ndk_home -android-sdk=$ANDROID_HOME \
+        -jdk-path=$JAVA_HOME \
         -projectPath $project_path \
         -collection=$build_collection
 else
@@ -11,6 +12,7 @@ else
         /opt/Unity/Editor/Unity -logfile -quit -batchmode -nographics -force-opengl \
             -executeMethod UBS.UBSProcess.BuildFromCommandLine \
             -android-ndk=$android_ndk_home -android-sdk=$ANDROID_HOME \
+            -jdk-path=$JAVA_HOME \
             -projectPath $project_path \
             -collection=$build_collection
 fi
