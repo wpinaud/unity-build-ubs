083d187f1b01c9abc9c4b89ab1bdf9f6b04007e6
Willis Pinaud <willispinaud@gmail.com>
2017-10-24 13:33:58 -0400

    Update step.sh

--- step.sh
+++ step.sh
@@ -2,15 +2,19 @@
 
 if [ "$(uname)" == "Darwin" ]; then
     /Applications/Unity/Unity.app/Contents/MacOS/Unity -logfile -quit -batchmode -nographics \
-        -executeMethod UBS.UBSProcess.BuildFromCommandLine \
+        -executeMethod LiveLike.UnityCloudBuildPrepare.BuildFromCommandLine \
+        -projectPath "$project_path" \
         -android-ndk=$android_ndk_home -android-sdk=$ANDROID_HOME \
-        -projectPath $project_path \
-        -collection=$build_collection
+        -jdk-path=$JAVA_HOME \
+        -configurationAssetPath="Assets/AllConfigurationFiles/FOXSportsVR/rsn_test_QA_Cardboard.asset" \
+
 else
     xvfb-run -a -n 55 -s "-screen 0 800x600x24 -ac +extension GLX +render -noreset" \
         /opt/Unity/Editor/Unity -logfile -quit -batchmode -nographics -force-opengl \
-            -executeMethod UBS.UBSProcess.BuildFromCommandLine \
+            -executeMethod LiveLike.UnityCloudBuildPrepare.BuildFromCommandLine \
+            -projectPath "$project_path" \
             -android-ndk=$android_ndk_home -android-sdk=$ANDROID_HOME \
-            -projectPath $project_path \
-            -collection=$build_collection
+            -jdk-path=$JAVA_HOME \
+            -configurationAssetPath="Assets/AllConfigurationFiles/FOXSportsVR/rsn_test_QA_Cardboard.asset" \
+
 fi
