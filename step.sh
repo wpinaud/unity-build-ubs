ce28f4040c58056390ac575b25bb45e7f8b59217
Willis Pinaud <willispinaud@gmail.com>
2017-10-25 15:29:23 -0400

    Update step.sh

--- step.sh
+++ step.sh
@@ -2,6 +2,7 @@
 
 if [ "$(uname)" == "Darwin" ]; then
     /Applications/Unity/Unity.app/Contents/MacOS/Unity -logfile -quit -batchmode -nographics \
+        -buildTarget android \
         -executeMethod LiveLike.UnityCloudBuildPrepare.BuildFromCommandLine \
         -projectPath "$project_path" \
         -android-ndk=$android_ndk_home -android-sdk=$ANDROID_HOME \
@@ -11,6 +12,7 @@ if [ "$(uname)" == "Darwin" ]; then
 else
     xvfb-run -a -n 55 -s "-screen 0 800x600x24 -ac +extension GLX +render -noreset" \
         /opt/Unity/Editor/Unity -logfile -quit -batchmode -nographics -force-opengl \
+            -buildTarget android \
             -executeMethod LiveLike.UnityCloudBuildPrepare.BuildFromCommandLine \
             -projectPath "$project_path" \
             -android-ndk=$android_ndk_home -android-sdk=$ANDROID_HOME \
