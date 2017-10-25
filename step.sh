c18a8c05201df0d48fe84509c5ae6fd371c66dba
Willis Pinaud <willispinaud@gmail.com>
2017-10-25 16:00:47 -0400

    Update step.sh

--- step.sh
+++ step.sh
@@ -2,21 +2,21 @@
 
 if [ "$(uname)" == "Darwin" ]; then
     /Applications/Unity/Unity.app/Contents/MacOS/Unity -logfile -quit -batchmode -nographics \
-        -buildTarget android \
+        -buildTarget $build_target \
         -executeMethod LiveLike.UnityCloudBuildPrepare.BuildFromCommandLine \
         -projectPath "$project_path" \
         -android-ndk=$android_ndk_home -android-sdk=$ANDROID_HOME \
         -jdk-path=$JAVA_HOME \
-        -configurationAssetPath="Assets/AllConfigurationFiles/FOXSportsVR/rsn_test_QA_Cardboard.asset" \
+        -configurationAssetPath="$configuration_asset_path" \
 
 else
     xvfb-run -a -n 55 -s "-screen 0 800x600x24 -ac +extension GLX +render -noreset" \
         /opt/Unity/Editor/Unity -logfile -quit -batchmode -nographics -force-opengl \
-            -buildTarget android \
+            -buildTarget $build_target \
             -executeMethod LiveLike.UnityCloudBuildPrepare.BuildFromCommandLine \
             -projectPath "$project_path" \
             -android-ndk=$android_ndk_home -android-sdk=$ANDROID_HOME \
             -jdk-path=$JAVA_HOME \
-            -configurationAssetPath="Assets/AllConfigurationFiles/FOXSportsVR/rsn_test_QA_Cardboard.asset" \
+            -configurationAssetPath="$configuration_asset_path" \
 
 fi
