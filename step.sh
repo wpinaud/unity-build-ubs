7451f94d3c1a3bb8dd8f430f7961942a4a015ccf
Willis Pinaud <willispinaud@gmail.com>
2017-10-29 14:24:18 -0400

    Update step.sh

--- step.sh
+++ step.sh
@@ -7,9 +7,7 @@ if [ "$(uname)" == "Darwin" ]; then
         -projectPath "$project_path" \
         -android-ndk=$android_ndk_home -android-sdk=$ANDROID_HOME \
         -jdk-path=$JAVA_HOME \
-        -configurationAssetPath="$configuration_asset_path" \
-             if [ $is_ios]; then "-isIos"; else ""; fi \
-             if [ $vr_enabled]; then "-vrEnabled"; else ""; fi
+        -configurationAssetPath="$configuration_asset_path" ; if [ $is_ios]; then "-isIos"; else ""; fi ; if [ $vr_enabled]; then "-vrEnabled"; else ""; fi
 
 else
     xvfb-run -a -n 55 -s "-screen 0 800x600x24 -ac +extension GLX +render -noreset" \
@@ -19,8 +17,5 @@ else
             -projectPath "$project_path" \
             -android-ndk=$android_ndk_home -android-sdk=$ANDROID_HOME \
             -jdk-path=$JAVA_HOME \
-            -configurationAssetPath="$configuration_asset_path" \
-             if [ $is_ios]; then "-isIos"; else ""; fi \
-             if [ $vr_enabled]; then "-vrEnabled"; else ""; fi
-
+            -configurationAssetPath="$configuration_asset_path" ; if [ $is_ios]; then "-isIos"; else ""; fi ; if [ $vr_enabled]; then "-vrEnabled"; else ""; fi
 fi
